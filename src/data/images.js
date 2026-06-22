00bf70c61f44b3426a42489666ec1c1a5a221611
Delice2004 <deliceuwamahoro10@gmail.com>
2026-06-22 11:33:13 +0200

    Add images data file

---
+++ src/data/images.js
@@ -0,0 +1,52 @@
+const sample = [
+  {
+    id: '1',
+    name: 'Gallery-1234725783.jpg',
+    url: 'https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=900&q=80&auto=format&fit=crop',
+    opened: 'just now'
+  },
+  {
+    id: '2',
+    name: 'Gallery-25783.jpg',
+    url: 'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?w=900&q=80&auto=format&fit=crop',
+    opened: '2 day ago'
+  },
+  {
+    id: '3',
+    name: 'Gallery-49895383.jpg',
+    url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=900&q=80&auto=format&fit=crop',
+    opened: 'a month ago'
+  },
+  {
+    id: '4',
+    name: 'Gallery-4509853.jpg',
+    url: 'https://images.unsplash.com/photo-1506929562872-bb421503ef21?w=900&q=80&auto=format&fit=crop',
+    opened: '2 day ago'
+  },
+  {
+    id: '5',
+    name: 'Gallery-280397557.jpg',
+    url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=900&q=80&auto=format&fit=crop',
+    opened: 'just now'
+  },
+  {
+    id: '6',
+    name: 'Gallery-76935783.jpg',
+    url: 'https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=900&q=80&auto=format&fit=crop',
+    opened: '2 day ago'
+  },
+  {
+    id: '7',
+    name: 'Gallery-4725783.jpg',
+    url: 'https://images.unsplash.com/photo-1500375592092-40eb2168fd21?w=900&q=80&auto=format&fit=crop',
+    opened: 'a month ago'
+  },
+  {
+    id: '8',
+    name: 'Gallery-4725783.jpg',
+    url: 'https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=900&q=80&auto=format&fit=crop',
+    opened: '2 day ago'
+  }
+]
+
+export default sample
